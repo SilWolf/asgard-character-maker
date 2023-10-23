150366f0b548ed675d9d0691b7aad69b6fe8b060
dicky.c <dicky.c@binance.com>
2023-10-23 13:33:49 +0800

    feat: minor metadata update

--- src/layouts/public.layout.tsx
+++ src/layouts/public.layout.tsx
@@ -6,18 +6,36 @@ const PublicLayout = ({ children }: Props) => {
   return (
     <div>
       <header className="p-3 bg-gray-500 border-b border-gray-600 mb-8 sticky top-0 z-10">
-        <div className="container mx-auto flex gap-x-4 justify-between items-center">
-          <div>
+        <div className="container mx-auto flex gap-x-8 justify-between items-center">
+          <div className="shrink-0">
             <h3 className="text-lg text-white">
               <a href="/">RPG公會創作模版工具</a>
             </h3>
           </div>
-          <div>
+          <div className="flex-1">
             <nav className="space-x-6 text-sm text-white">
+              <a href="#">首頁</a>
+              <a href="#">範例角色卡＆模組</a>
               <a href="#">如何使用？</a>
-              <a href="#">RPG公會</a>
-              <a href="#">對串插件</a>
-              <a href="#">沙鷗擲骰網</a>
+            </nav>
+          </div>
+          <div className="shrink-0">
+            <nav className="space-x-6 text-sm text-white">
+              <a
+                href="https://guild.gamer.com.tw/about.php?gsn=3014"
+                target="_blank"
+              >
+                RPG公會
+              </a>
+              <a
+                href="https://github.com/SilWolf/bahamut-guild-v2-toolkit"
+                target="_blank"
+              >
+                對串插件
+              </a>
+              <a href="https://www.isaka.idv.tw/dice/" target="_blank">
+                沙鷗擲骰網
+              </a>
             </nav>
           </div>
         </div>
