bedce13421d1e835e8aeb93e9b6715c6c33bb19d
dicky.c <dicky.c@binance.com>
2023-10-24 20:43:30 +0800

    feat: minor update for marketplace

--- src/components/MarketplaceItemCard.tsx
+++ src/components/MarketplaceItemCard.tsx
@@ -1,18 +1,34 @@
 import { BahaTemplateProperties } from "@/types/Baha.type";
+import { useMemo } from "react";
 
 type Props = {
   properties: BahaTemplateProperties;
 };
 
 const MarketplaceItemCard = ({ properties }: Props) => {
+  const tags = useMemo(() => properties.tags.split(","), [properties.tags]);
+
   return (
-    <div className="space-y-2">
+    <div className="space-y-4">
       <img
         className="aspect-square bg-gray-200"
         src={properties.previewImageUrl}
       />
-      <h5 className="text-lg">{properties.name}</h5>
+      <div>
+        <h5 className="text-lg">{properties.name}</h5>
+        <p className="text-sm text-gray-600">作者: {properties.author}</p>
+      </div>
       <p className="text-sm text-gray-600">{properties.briefing}</p>
+      <div className="space-x-1 text-xs text-white">
+        {tags.map((tag) => (
+          <div
+            key={tag}
+            className="inline-block bg-gray-500 rounded-full px-3 py-1"
+          >
+            {tag}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
--- src/pages/marketplace/index.page.tsx
+++ src/pages/marketplace/index.page.tsx
@@ -30,7 +30,7 @@ const MarketplacePage = () => {
         <section className="container mx-auto space-y-2">
           <h1 className="text-3xl">範例模版</h1>
           <p>
-            這裡取錄了預設的角色卡及模版以供複製使用，推薦初次到來的玩家，從這裡挑選心宜的模版來製作你的角色卡
+            這裡取錄了預設的角色卡及模版以供複製使用，推薦初次到來的玩家，從這裡挑選心宜的模版來製作你的角色卡。
           </p>
         </section>
 
